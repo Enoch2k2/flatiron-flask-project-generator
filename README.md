c6b0c3366373a85fd41fbdc98a284239d06237ac
Enoch2k2 <enoch2k2@hotmail.com>
2023-12-06 13:31:05 -0600

    added .env, .gitignore, instructions for proxy with vite

--- README.md
+++ README.md
@@ -16,7 +16,23 @@ You're Flask project is ready to go! Create models / routes / etc... You will no
 
 You also now have a client folder which is your React application. You can definitely use it if you want (if it this is your project, you will want to use this). This includes react-router-dom, formik, and yup automatic installations.
 
-You will need to go to `package.json` in your client folder and above the `"scripts"` option, you will need to add: `"proxy": "http:127.0.0.1:5555",`.
+You will need to go to `vite.config.js` in your client folder and inside the `defineConfig` under the `plugins: [react()],` line, you will need to add:
+
+```
+proxy: {
+      '/api': 'http://localhost:5555'
+    },
+```
+
+This will mean anytime you do a fetch with `/api` at the start, it will proxy to `http://localhost:5555`. Example:
+
+```
+fetch('/api/check_session')
+```
+
+The /api will trigger the proxy and will send the request to `http://localhost:5555`.
+
+Note: Hoping to get this automated in the future. Just not there yet.
 
 ### If you run into issue with flask-bcrypt installing because of a rust issue
 
--- script.sh
+++ script.sh
@@ -1,16 +1,18 @@
 #!/bin/zsh
 
 # argumentvariables
-secret_key=$1
+secret_key=`hexdump -vn16 -e'4/4 "%08X" 1 "\n"' /dev/urandom`
 
 # setup virtual environment and add dependencies
 pipenv install --python 3.9.2
-pipenv install flask flask-sqlalchemy sqlalchemy-serializer flask-cors flask-restful flask-migrate flask-bcrypt
+pipenv install flask flask-sqlalchemy sqlalchemy-serializer flask-cors flask-restful flask-migrate flask-bcrypt python-dotenv
 
 # filestructures
 mkdir server
 touch server/app.py
 touch server/config.py
+touch .env
+touch .gitignore
 
 # imports config.py
 echo "from flask import Flask" >> server/config.py
@@ -19,9 +21,13 @@ echo "from sqlalchemy import MetaData" >> server/config.py
 echo "from flask_migrate import Migrate" >> server/config.py
 echo "from flask_bcrypt import Bcrypt" >> server/config.py
 echo "from flask_restful import Api" >> server/config.py
-echo "from flask_cors import CORS\n" >> server/config.py
+echo "from flask_cors import CORS" >> server/config.py
+echo "from dotenv import load_dotenv" >> server/config.py
+echo "import os\n" >> server/config.py
 
 # configuring config.py
+echo "load_dotenv()\n" >> server/config.py
+
 echo "naming_convention = {
     \"ix\": \"ix_%(column_0_label)s\",
     \"uq\": \"uq_%(table_name)s_%(column_0_name)s\",
@@ -31,8 +37,8 @@ echo "naming_convention = {
 }\n" >> server/config.py
 echo "metadata = MetaData(naming_convention=naming_convention)\n" >> server/config.py
 echo "app = Flask(__name__)" >> server/config.py
-echo "app.secret_key = \"$secret_key\"" >> server/config.py
-echo "app.config[\"SQLALCHEMY_DATABASE_URI\"] = \"sqlite:///app.db\"" >> server/config.py
+echo "app.secret_key = os.getenv(\"FLASK_SECRET_KEY\")" >> server/config.py
+echo "app.config[\"SQLALCHEMY_DATABASE_URI\"] = os.getenv(\"DATABASE_URI\")" >> server/config.py
 echo "app.config[\"SQLALCHEMY_TRACK_MODIFICATIONS\"] = False\n" >> server/config.py
 echo "db = SQLAlchemy(app=app, metadata=metadata)\n" >> server/config.py
 echo "migrate = Migrate(app=app, db=db)\n" >> server/config.py
@@ -47,6 +53,14 @@ echo "from config import app\n" >> server/app.py
 echo "if __name__ == \"__main__\":" >> server/app.py
 echo "  app.run(port=5555, debug=True)" >> server/app.py
 
+# configuring .gitignore
+echo ".env" >> .gitignore
+
+# configuring .env
+echo "FLASK_SECRET_KEY=$secret_key" >> .env
+echo "DATABASE_URI=sqlite:///app.db" >> .env
+
+# initializing database
 cd server
 
 pipenv run flask db init
@@ -55,6 +69,7 @@ pipenv run flask db upgrade head
 
 cd ..
 
+# creating react client
 npm create vite@latest client -- --template react
 cd client
 npm install react-router-dom formik yup
